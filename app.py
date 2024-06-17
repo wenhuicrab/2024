1ba9eedf2d183e4b0edc5735d0800e8eda407162
wenhuicrab <w871811@gmail.com>
2024-06-17 17:30:54 +0800

    123

--- app.py
+++ app.py
@@ -16,7 +16,8 @@
 import os
 import sys
 import phonetic as ph
-import 99 
+import aaaaa as a99 
+import random
 from argparse import ArgumentParser
 
 from flask import Flask, request, abort
@@ -45,6 +46,36 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
+def multiplication_quiz():
+    correct_count = 0  # 初始化正確答題計數器
+    
+    while correct_count < 10:  # 當正確答題計數器小於 10 時持續出題
+        # 隨機選擇兩個數字作為題目
+        num1 = random.randint(1, 9)
+        num2 = random.randint(1, 9)
+        
+        # 計算正確答案
+        correct_answer = num1 * num2
+        
+        while True:  # 使用內部迴圈處理輸入錯誤的情況
+            try:
+                # 提示題目並讀取使用者的回答
+                user_answer = int(input(f"{num1} * {num2}是多少? "))
+                
+                # 檢查答案是否正確
+                if user_answer == correct_answer:
+                    print("恭喜你答對了!")
+                    correct_count += 1  # 正確答題計數器加一
+                    print(f"已經答對了 {correct_count} 題！\n")
+                    break  # 跳出內部迴圈，出下一題
+                else:
+                    print("嗯...再多想想答案吧\n")
+                    
+            except ValueError:
+                print("請輸入有效的數字!")
+    
+    # 當答對十題時，顯示鼓勵訊息
+    print("恭喜你成功答對十題，做得很好！")
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -71,43 +102,16 @@ def callback():
             event.reply_token,
             TextSendMessage(text=ph.read(event.message.text));
         )
-
-    return 'OK'
-
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=ph.read(event.message.text));
-        )
-
-        # Call your multiplication quiz function here
-        elif event.message.text == '九九乘法表':
+          # Call your multiplication quiz function here
+        elif event.message.text == '九九乘法表':  # 假設觸發條件是收到文字訊息 '九九乘法表'
             multiplication_quiz(event.reply_token)
-            else:
-                line_bot_api.reply_message(
-                    event.reply_token,
+             else:
+                 line_bot_api.reply_message(
+                     event.reply_token,
                     TextSendMessage(text="請輸入 '九九乘法表' 來觸發九九乘法表")
-        )
+            )
         
+      
 
 
     return 'OK'
