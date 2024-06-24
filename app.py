8d3ae4128f6db16e29189a5fe52e82fdce419fb3
wenhuicrab <118326302+wenhuicrab@users.noreply.github.com>
2024-06-24 15:50:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-import random
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+
+import os
+import sys
+import phonetic as ph
 import datetime
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, request, abort, jsonify
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookParser
 )
@@ -10,14 +21,23 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, StickerSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
 )
-import phonetic as ph  # 假設 ph.read 函式在 phonetic 模組中
-import settings  # 假設 LINE_CHANNEL_ACCESS_TOKEN 和 LINE_CHANNEL_SECRET 在 settings 中設定
 
+app = Flask(__name__)
 
-line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
 
 # 全局變數來跟踪乘法測驗的狀態
 multiplication_ing = False
