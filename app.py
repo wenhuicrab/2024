35339bec5218d74587372f86b8d51b705f45d8c5
wenhuicrab <118326302+wenhuicrab@users.noreply.github.com>
2024-06-24 15:35:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -98,31 +98,70 @@ def getNews(num=10):
     return mm
 
 @app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
+def callback(request):
+    global multiplication_ing, num1, num2, correct_answer
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
 
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
 
-    # handle webhook events
-    for event in events:
-        if isinstance(event, MessageEvent):
-            msg = event.message.text
-            
-            if msg == '統一發票':
-                reply_msg = getInvoice()
-            elif msg.startswith('/'):
-                reply_msg = cambridge(msg[1:])
-            elif msg in ['最新消息', '今日新聞']:
-                reply_msg = getNews(6)
-             elif msg == '九九乘法':
+        for event in events:
+            # 若有訊息事件
+            if isinstance(event, MessageEvent):
+                msg = event.message.text
+                imgurl = "https://i.imgur.com/6hVi7dy.gif"
+
+                if msg in ['hello', 'hi', '嗨', '哈囉']:
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        StickerSendMessage(package_id=11537, sticker_id=52002738)
+                    )
+
+                elif msg == 'guess':
+                    num = random.randint(1, 10)
+                    reply_msg = f"{num}"
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=reply_msg)
+                    )
+
+                elif msg.startswith('/'):
+                    reply_msg = cambridge(msg[1:])
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=reply_msg)
+                    )
+
+                elif msg in ['最新消息', '今日新聞']:
+                    reply_msg = getNews(6)
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=reply_msg)
+                    )
+
+                elif msg == '統一發票':
+                    reply_msg = getInvoice()
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=reply_msg)
+                    )
+
+                elif msg in ['求籤', '抽籤']:
+                    num = random.randint(1, 100)
+                    img = f"https://www.lungshan.org.tw/fortune_sticks/images/{num:03d}.jpg"
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        ImageSendMessage(original_content_url=img, preview_image_url=img)
+                    )
+
+                elif msg == '九九乘法':
                     if multiplication_ing:
                         multiplication_ing = False
                         reply_msg = "測驗結束"
@@ -169,17 +208,13 @@ def callback():
                         TextSendMessage(text=reply_msg)
                     )
 
-            else:
-                # Default response
-                tdnow = datetime.datetime.now()
-                reply_msg = tdnow.strftime("%Y/%m/%d, %H:%M:%S") + '\n' + event.message.text
-            
-            # Reply to the user
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=reply_msg)
-            )
-
+                else:
+                    tdnow = datetime.datetime.now()
+                    reply_msg = tdnow.strftime("%Y/%m/%d, %H:%M:%S") + '\n' + event.message.text
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=reply_msg)
+                    )
     return 'OK'
 
 if __name__ == "__main__":
